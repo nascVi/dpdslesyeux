b23c45ddb79bfd243667d487d1aaf46b2add9df7
nascvi <vaiiho.nascimbene@gmail.com>
2020-12-23 10:35:17 +0100

    repo created for official cci

---
+++ store/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+
+def store(request):
+    products = Product.objects.all()
+    context = {'products':products}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+    context = {'items':items}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
